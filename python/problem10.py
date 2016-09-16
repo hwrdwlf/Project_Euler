c982fe5f41b9692ce509e55e8614a3b9bc0da897
Howard Wolf <hwrdwlf@gmail.com>
2016-09-16 14:29:17 -0700

    updating solution using the sieve of Eratosthenes

--- python/problem10.py
+++ python/problem10.py
@@ -1,23 +1,26 @@
-#!/usr/bin/python
-#
-# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-#
-# Find the sum of all the primes below two million.
+'''
+Problem 10, https://projecteuler.net/problem=10
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
-import math
-
-prime = []
-for n in range(2,10):
-   for m in range(2,n/2):
-      if n%2 != 0:
-         if n%m != 0:
-            print "value: " , n
-            print "value: " , m
-            prime.append(n)
+Find the sum of all the primes below two million.
+'''
 
 
-print list(set(prime))
+limit = 2000000
+sievebound = (limit - 1) / 2
+crosslimit = int(math.sqrt(limit) - 1) / 2
+sieve = [0] * sievebound
 
+for i in range(1, crosslimit):
+	if sieve[i] == 0:
+		for j in range(2*i*(i+1), sievebound, 2*i+1):
+			sieve[j] = 1
+
+sum = 2
+for i in range(1, sievebound):
+	if sieve[i] == 0:
+		sum = sum + (2*i+1)
+print sum
 
 
 
