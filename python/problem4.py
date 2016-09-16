6df20e8673e905fb54e75c9fdc25c744e3bbb9dc
Howard Wolf <hwrdwlf@gmail.com>
2016-09-15 17:59:12 -0700

    updating solutions

--- python/problem4.py
+++ python/problem4.py
@@ -1,27 +1,69 @@
 #!/usr/bin/python
 
-# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 * 99.
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
-def checkPalindrome(num):
-	string = str(num)
-	if string == string[::-1]:
-		return True
-	else:
-		return False
+def reverse(n):
+	reversed = 0
+	while n > 0:
+		reversed = 10*reversed + n % 10
+		n = n/10
 
+	return reversed
 
-list = []
-total = 0
-for a in range(1,1000):
-	for b in range(1,1000):
-		total = b * a
-		if checkPalindrome(total):
-			list.append(total)
-def reverseNum(x,y):
-	return y - x
+def is_palindrome(n):
+	return n == reverse(n)
 
-test = sorted(list, cmp = reverseNum)
-print "ans:", test[0]
+# Consider the digits of P: let them be x, y and z. 
+# P must be at least 6 digits long since the palindrome 111111 = 143 x 777 minus the product of two 3-digit integers. 
+# Since P is palindromic:
+# P=100000x+10000y+1000z+100z+10y+x
+# P=100001x+10010y+1100z
+# P=11(9091x+910y+100z)
+# Since 11 is prime, at least one of the integers a or b must have a factor of 11.
+# So if a is not divisible by 11 then we know b must be. Using this information
+# we can determine what values of b we check depending on a.
+
+def super_fast():
+	largest_pali = 0
+	a = 999
+	b = 0
+	db = 1
+
+	while a >= 100:
+		if a % 11 == 0:
+			b = 999
+			db = 1
+		else:
+			b = 990
+			db = 11
+		while b >= a:
+			if a*b <= largest_pali:
+				break
+
+			if is_palindrome(a*b):
+				largest_pali = a*b
+			b = b - db
+		a = a - 1
+
+	return largest_pali
+
+def solve():
+	largest_pali = 0
+	a = 999
+	b = 0
+	while a >= 100:
+		b = 999
+		while b >= a:
+			if a*b <= largest_pali:
+				break
+			if is_palindrome(a*b):
+				largest_pali = a*b
+			b = b -1
+		a = a - 1
+	return largest_pali
+
+print solve()
+# print super_fast()
+
 
--- python/problem5.py
+++ python/problem5.py
@@ -3,26 +3,46 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-from time import clock,time
+import math
 
 
-def GCD(num1, num2):
-	while num2 != 0:
-		num1, num2 = num2, num1%num2
-		return num1
-def LCM(num1, num2):
-	result = num1*num2/GCD(num1,num2)
-	return result
+def primes(n):
+    """ Returns  a list of primes < n """
+    sieve = [True] * n
+    for i in xrange(3,int(n**0.5)+1,2):
+        if sieve[i]:
+            sieve[i*i::2*i]=[False]*((n-i*i-1)/(2*i)+1)
+    return [2] + [i for i in xrange(3,n,2) if sieve[i]]
 
-start = time()
-var1 = 2520
-i = 1
-while i < 21:
-	if var1%i == 0:
-		i = i+1
+# k = 20
+# n = 1
+# i = 1
+# check = 1
+# prime_list = primes(40)
+# a = 0
+# limit = math.sqrt( k )
+
+# while prime_list[i] <= k:
+# 	a = 1
+# 	if check:
+# 		if prime_list[i] <= limit:
+# 			a = math.floor( math.log(k) / math.log(prime_list[i]) )
+# 		else:
+# 			check = 0
+# 	n = n * math.pow( prime_list[i], a )
+# 	i = i + 1
+
+# print n
+def gcd(a,b):
+	if a < b:
+		return gcd(b,a)
+	if a % b == 0:
+		return b
 	else:
-		var1=var1+10
-		i=1
-end = (time() - start)
-print var1
-print "Time elapsed:",end
+		return gcd(b, a%b)
+
+def lcm(a,b):
+	return abs(a*b)/gcd(a,b)
+
+n = 20
+print reduce(lambda res, x: lcm(res,x), range(n, 1, -1), 1)
